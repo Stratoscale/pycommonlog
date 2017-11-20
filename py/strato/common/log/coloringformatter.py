6e3c3ff6592b69ecb9f374e17c58993d03d88684
Ruslan Portnoy <ruslan.portnoy@stratoscale.com>
2017-11-20 17:46:52 +0200

    added colors

--- py/strato/common/log/coloringformatter.py
+++ py/strato/common/log/coloringformatter.py
@@ -1,5 +1,6 @@
 import logging
 import strato.common.log.morelevels
+from strato.common.log import config
 
 _INFO = logging.INFO
 _PROGRESS = logging.PROGRESS
@@ -34,17 +35,19 @@ class ColoringFormatter(Formatter):
     _YELLOW = '\033[33m'
     _CYAN = '\033[36m'
     _LIGHT_CYAN = '\033[96m'
+    _LIGHT_MAGENTA = '\033[95m'
+    _LIGHT_BLUE = '\033[1;34m'
     _LIGHT_GREEN = '\033[92m'
     _SALMON = '\033[91m'
     _NORMAL_COLOR = '\033[39m'
 
     STEP_COUNT = 1
 
-    def _update_colors(self, record):
+    def __update_colors(self, record):
         record.endColor = self._NORMAL_COLOR
         if record.levelno == _INFO:
-            record.startColor = self._LIGHT_CYAN
-        if record.levelno == _PROGRESS:
+            record.startColor = self._LIGHT_BLUE
+        elif record.levelno == _PROGRESS:
             record.startColor = self._CYAN
         elif record.levelno == _SUCCESS:
             record.startColor = self._GREEN
@@ -56,6 +59,25 @@ class ColoringFormatter(Formatter):
             self.STEP_COUNT += 1
         elif record.levelno == _ERROR or record.levelno == _CRITICAL:
             record.startColor = self._RED
+        else:
+            record.startColor = ""
+
+    def _update_colors(self, record):
+        record.endColor = config.COLORS['REGULAR']
+        if record.levelno == _INFO:
+            record.startColor = config.COLORS['LIGHT_BLUE']
+        elif record.levelno == _PROGRESS:
+            record.startColor = config.COLORS['CYAN']
+        elif record.levelno == _SUCCESS:
+            record.startColor = config.COLORS['GREEN']
+        elif record.levelno == _WARNING:
+            record.startColor = config.COLORS['YELLOW']
+        elif record.levelno == _STEP:
+            record.startColor = config.COLORS['LIGHT_GREEN']
+            record.levelname = "STEP [%d]" % self.STEP_COUNT
+            self.STEP_COUNT += 1
+        elif record.levelno == _ERROR or record.levelno == _CRITICAL:
+            record.startColor = config.COLORS['RED']
         else:
             record.startColor = ""
 
